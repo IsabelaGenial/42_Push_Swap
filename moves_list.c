c740b2b4b6c4f9ea624de5469cb9954f01767abc
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-10-25 16:21:12 -0300

    fix(moves_list.c): creation of doubly circular linked list is working.

--- moves_list.c
+++ moves_list.c
@@ -15,14 +15,14 @@
 void create_list_if_str(t_stack *stack, char **args)
 {
 	int i;
-	t_node node;
+	t_node *node;
 	int content;
 
 	i = 1;
 	while (args[i])
 	{
 		content = ft_atoi(args[i++]);
-		node = *DCLL_lstnew(content);
-		DCLL_lst_add_back(stack->stack_a, &node);
+		node = DCLL_lstnew(content);
+		DCLL_lst_add_back(node, stack);
 	}
 }
