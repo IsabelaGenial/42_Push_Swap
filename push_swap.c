81139b21ccb0a60401b017b09ab8aa60c08afd25
Isabela Genial <isabelamegenial@gmail.com>
2023-11-06 19:51:37 -0300

    feat: PB, SB, RRB and RB were implemented

--- push_swap.c
+++ push_swap.c
@@ -12,35 +12,40 @@
 
 #include "push_swap.h"
 
-int main(int argc, char **arg )
+int     main(int argc, char **arg )
 {
-	t_stack *stack;
+	t_main *stack;
 
 	int len = argc;
 
 	if (argc < 6) {
 		return (ft_printf("error\n"));
 	}
-	stack = ft_calloc(1, sizeof(t_stack));
+	stack = ft_calloc(1, sizeof(t_main));
+    stack->A = ft_calloc(1, sizeof(t_stack));
+    stack->B = ft_calloc(1, sizeof(t_stack));
 	create_list_if_str(stack, arg);
-	t_node *start_tail = stack->tail;
-	t_node *start_head = stack->head;
+	t_node *start_tail = stack->A->tail;
+	t_node *start_head = stack->A->head;
 	while (len-- >= 2)
 	{
 		ft_printf("%i | %i\n", start_head->content,  start_tail->content);
 		start_tail = start_tail->prev;
 		start_head = start_head->next;
 	}
-	SA_DCLL_list(stack);
-	start_tail = stack->tail;
-	start_head = stack->head;
-	len = argc;
-	while (len-- >= 2)
+	PB_DCLL_list(stack);
+    PB_DCLL_list(stack);
+    PB_DCLL_list(stack);
+    PB_DCLL_list(stack);
+	start_tail = stack->A->tail;
+	start_head = stack->A->head;
+    t_node *start_b = stack->B->head;
+	len = argc/2;
+	while (len-- >= 1)
 	{
-		ft_printf("*%i | %i\n", start_head->content,  start_tail->content);
-		start_tail = start_tail->prev;
-		start_head = start_head->next;
-	}
-
+		ft_printf("*%i | %i\n", start_head->content,  start_b->content);
+        start_b = start_b->next;
+        start_head = start_head->next;
+    }
 	return(0);
 }
