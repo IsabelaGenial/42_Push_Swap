a7375f87f6599dff33d06c3faf7aa94a54f24f3a
Isabela Genial <isabelamegenial@gmail.com>
2023-11-10 11:00:51 -0300

    feat: implementeion of implementation of the sort lgorithm

--- push_swap.c
+++ push_swap.c
@@ -16,36 +16,13 @@ int     main(int argc, char **arg )
 {
 	t_main *stack;
 
-	int len = argc;
-
 	if (argc < 6) {
 		return (ft_printf("error\n"));
 	}
 	stack = ft_calloc(1, sizeof(t_main));
     stack->A = ft_calloc(1, sizeof(t_stack));
     stack->B = ft_calloc(1, sizeof(t_stack));
+    stack->len = (argc - 2);
 	create_list_if_str(stack, arg);
-	t_node *start_tail = stack->A->tail;
-	t_node *start_head = stack->A->head;
-	while (len-- >= 2)
-	{
-		ft_printf("%i | %i\n", start_head->content,  start_tail->content);
-		start_tail = start_tail->prev;
-		start_head = start_head->next;
-	}
-	PB_DCLL_list(stack);
-    PB_DCLL_list(stack);
-    PB_DCLL_list(stack);
-    PB_DCLL_list(stack);
-	start_tail = stack->A->tail;
-	start_head = stack->A->head;
-    t_node *start_b = stack->B->head;
-	len = argc/2;
-	while (len-- >= 1)
-	{
-		ft_printf("*%i | %i\n", start_head->content,  start_b->content);
-        start_b = start_b->next;
-        start_head = start_head->next;
-    }
 	return(0);
 }
