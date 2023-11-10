b0ac39a18ca7ae308c98c58bdf3fbe2434c08614
Isabela Genial <isabelamegenial@gmail.com>
2023-11-10 10:59:49 -0300

    feat: implementeion of implementation of the sort lgorithm

--- moves_list_a.c
+++ moves_list_a.c
@@ -60,26 +60,26 @@ void SA_DCLL_list(t_main *stack)
 void PA_DCLL_list(t_main *stack)
 {
     t_node *temp;
-    temp = stack->A->head;
-    if (stack->B->head == NULL)
+    temp = stack->B->head;
+    if (stack->A->head == NULL)
     {
-        stack->A->head = stack->A->head->next;
-        stack->A->head->prev = stack->A->tail;
-        stack->A->tail->next = stack->A->head;
-        stack->B->head = temp;
-        stack->B->head->next = stack->B->head;
-        stack->B->head->prev = stack->B->head;
+        stack->B->head = stack->B->head->next;
+        stack->B->head->prev = stack->B->tail;
+        stack->B->tail->next = stack->B->head;
+        stack->A->head = temp;
+        stack->A->head->next = stack->A->head;
+        stack->A->head->prev = stack->A->head;
     }
     else
     {
-        stack->A->head = stack->A->head->next;
-        stack->A->head->prev = stack->A->tail;
-        stack->A->tail->next = stack->A->head;
-        stack->B->tail = stack->B->head->prev;
-        temp->next = stack->B->head;
-        temp->prev = stack->B->tail;
-        stack->B->tail->next = temp;
-        stack->B->head->prev = temp;
-        stack->B->head = temp;
+        stack->B->head = stack->B->head->next;
+        stack->B->head->prev = stack->B->tail;
+        stack->B->tail->next = stack->B->head;
+        stack->A->tail = stack->A->head->prev;
+        temp->next = stack->A->head;
+        temp->prev = stack->A->tail;
+        stack->A->tail->next = temp;
+        stack->A->head->prev = temp;
+        stack->A->head = temp;
     }
 }
